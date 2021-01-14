fe26bb3825622650833de6e1c8c64cd09126c929
richardykedang <kedangrichard@gmail.com>
2021-01-14 15:47:10 +0700

    Add Navbar component

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//import bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+//import jquery popper
+import 'bootstrap'
 
 createApp(App).use(store).use(router).mount('#app')
