7116038a08656d18efecc1b6fa78f04f5f1de992
richardykedang <kedangrichard@gmail.com>
2021-01-15 13:48:03 +0700

    define route view on router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/authentication/Login.vue'
+import Register from '../views/authentication/Register.vue'
+import TaskAll from '../views/task/TaskAll.vue'
+import TaskCreate from '../views/task/TaskCreate.vue'
+import TaskEdit from '../views/task/TaskEdit.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/task/new',
+    name: 'task-create',
+    component: TaskCreate
+  },
+  {
+    path: '/task/:id',
+    name: 'task-edit',
+    component: TaskEdit
+  },
+  {
+    path: '/task',
+    name: 'task-all',
+    component: TaskAll
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
     path: '/about',
